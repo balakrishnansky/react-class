041adb9f3657f2e237a0e40ae630fcdb64281c4f
balakrishnansky <balakrishnan.sky@gmail.com>
2020-11-22 03:47:26 -0800

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 import "./style.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: "Balakrishnan Ramalingam",
-      age: "40",
-      country: "USA"
+      name: "Balakrishnan",
+      age: 55,
+      loggenIn: false
     };
   }
   render() {
     return (
       <div>
-        <h1>{this.state.name}</h1>
-        <h3>{this.state.age}</h3>
-        <h3>{this.state.country}</h3>
+        <h1>
+          your are now logged {this.state.loggenIn ? "IN" : "OUT"} as{" "}
+          {this.state.name}
+        </h1>
       </div>
     );
   }
