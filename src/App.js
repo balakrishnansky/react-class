7b87a6aec31b9e6e76a2bb842099179a0d0e0d3a
balakrishnansky <balakrishnan.sky@gmail.com>
2020-11-23 16:12:29 -0800

    3 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
 import React, { Component } from "react";
+import FormContainer from "./components/FormContainer";
 import "./style.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0
-    };
-    this.hanldeClickEvent = this.hanldeClickEvent.bind(this);
-  }
-  hanldeClickEvent() {
-    this.setState(prevState => {
-      return {
-        count: prevState.count + 3
-      };
-    });
-  }
-
-  render() {
-    return (
-      <div className="placeCenter">
-        <h1>{this.state.count} </h1>
-        <button onClick={this.hanldeClickEvent}>Click me</button>
-      </div>
-    );
-  }
+function App() {
+  return <FormContainer />;
 }
-
 export default App;
